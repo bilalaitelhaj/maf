5ae605db202866de282c5b47726ebe085c239b50
bilalaitelhaj <bilal.aitelhaj@lycee-jeanrostand.fr>
2025-09-20 12:24:22 +0200

    Supprime les fichiers du dépôt

--- subordinate.py
+++
@@ -1,9 +0,0 @@
-class Subordinate:
-    def __init__(self, name, role, power, cost):
-        self.name = name
-        self.role = role
-        self.power = power
-        self.cost = cost
-
-    def __str__(self):
-        return f"{self.name} ({self.role}) - Puissance: {self.power}, Coût: {self.cost}"
